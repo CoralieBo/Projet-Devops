a46918e177d7e43f7acaa25b093c1fb499c14863
Coralie <coralie@digitalcube.net>
2023-11-24 17:07:31 +0100

    try to push workflows

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Bonjour, monde test!');
+    res.send('Bonjour, monde !');
 });
 
 app.listen(port, () => {
